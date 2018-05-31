f5726de9bed50fe65d98ed4774ee2f559ce00d5f
Yusuf Kaan Karakaya <yusufkaan142@gmail.com>
2018-06-01 00:08:46 +0300

    Ilk

---
+++ src/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * @ Package: Config - simple config class for php
+ * @ Class: Config
+ * @ Author: Yusuf Kaan Karakaya / @TheYkk <yusufkaan142@gmail.com>
+ * @ Web: http://theykk.net
+ * @ URL: https://github.com/TheYkk/config
+ * @ Licence: The MIT License (MIT) - Copyright (c) - http://opensource.org/licenses/MIT
+ *
+ */
+
+namespace TheYkk;
+
+
+class Config
+{
+
+}
